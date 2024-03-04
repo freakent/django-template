970fe54e7cb3beb6ccc200357b08be4f8fec28c3
= <=>
2024-03-04 16:53:22 +0000

    Datavase setup

--- template/template/settings.py
+++ template/template/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'template.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "OPTIONS": {
+            "service": "django-dev",
+            "passfile": ".my_pgpass"
+        }
     }
 }
 
