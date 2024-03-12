aad849a7cf87fd63509378cb16dee5d7e494275d
= <=>
2024-03-12 09:55:32 +0000

    add defaults to created on and modified on

--- application/common/models.py
+++ application/common/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
 
 class BaseModel(models.Model):
-    created_on = models.DateTimeField(auto_now_add = True)
+    created_on = models.DateTimeField(default= timezone.now(), auto_now_add = True)
     created_by = models.CharField(default="ANON")
 
-    modified_on = models.DateTimeField(auto_now = True)
+    modified_on = models.DateTimeField(default= timezone.now(), auto_now = True)
     modified_by = models.CharField(default="ANON")
 
     class Meta:
