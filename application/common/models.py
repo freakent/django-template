c4500d13cff76bb3f706384c555624b4767ec02f
= <=>
2024-03-12 09:30:28 +0000

    Added a base model abstract class

--- application/common/models.py
+++ application/common/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add = True)
+    created_by = models.CharField()
+
+    modified_on = models.DateTimeField(auto_now = True)
+    modified_by = models.CharField()
+
+    class Meta:
+        abstract = True
